2a31403ae6a346a252ce21fdb5b3facce0fac64a
JoKyuHoon <rbgnsl56@naver.com>
2025-02-05 10:40:09 +0900

    0204

--- 03.function_tjoeun계정.sql
+++ 03.function_tjoeun계정.sql
@@ -178,6 +178,328 @@ FROM EMPLOYEE;
  SELECT EMP_NAME, EMAIL, REPLACE(EMAIL,'tjoeun.or.kr', 'naver.com')
  FROM EMPLOYEE;
  
- 
+ --==============================================================
+--                                                      <숫자 처리 함수>
+--==============================================================
+/*
+    * ABS : 숫자의 절대값을 구해주는 함수
+    
+     [표현법]
+     ABS(NUMBER)
+*/
+SELECT ABS(-10) FROM DUAL;
+SELECT ABS(-34.567) FROM DUAL;
+
+---------------------------------------------------------------------------------------------------------
+/*
+    * MOD : 두 수를 나눈 나머지값을 반환해주는 함수
+    
+     [표현법]
+      MOD(NUMBER, NUMBER)
+*/
+
+SELECT MOD(10, 3) FROM DUAL;
+
+---------------------------------------------------------------------------------------------------------
+/*
+    * ROUND : 반올림한 결과를 반환해주는 함수
+    
+    [표현법]
+    ROUND(NUMBER, [위치])
+*/
+
+SELECT ROUND(1234.567) FROM DUAL;  -- 위치 생략시 0
+SELECT ROUND(12.345) FROM DUAL;
+SELECT ROUND(1234.56789, 2) FROM DUAL;
+SELECT ROUND(12.34, 4) FROM DUAL;
+SELECT ROUND(1234.5678, -2) FROM DUAL;
+SELECT ROUND(1734.5678, -3) FROM DUAL;
+
+---------------------------------------------------------------------------------------------------------
+/*
+    * CEIL : 무조건 올림
+    
+      [표현법]
+      CEIL(NUMBER)
+*/
+SELECT CEIL(123.45) FROM DUAL;
+SELECT CEIL(-123.45) FROM DUAL;
+
+---------------------------------------------------------------------------------------------------------
+/*
+    * FLOOR : 무조건 내림
+    
+      [표현법]
+      FLOOR(NUMBER)
+*/
+SELECT FLOOR(123.45) FROM DUAL;
+SELECT FLOOR(-123.45) FROM DUAL;
+
+---------------------------------------------------------------------------------------------------------
+/*
+    * TRUNC : 위치 지정 가능한 버림처리 함수
+    
+    [표현법]
+    TRUNC(NUMBER, [위치])
+*/
+
+SELECT TRUNC(123.789) FROM DUAL;
+SELECT TRUNC(123.789, 1) FROM DUAL;
+SELECT TRUNC(123.789, -1) FROM DUAL;
+
+SELECT TRUNC(-123.789) FROM DUAL;
+SELECT TRUNC(-123.789, -2) FROM DUAL;
+
+--==============================================================
+--                                                      <날짜 처리 함수>
+--==============================================================
+/*
+    * SYSDATE : 시스템 날짜 및 시간 반환
+*/
+SELECT SYSDATE FROM DUAL;
+
+---------------------------------------------------------------------------------------------------------
+/*
+    * MONTHS_BETWEEN(DATE1, DATE2) : 두 날짜 사이의 개월 수 반환
+*/
+-- EMPLOYEE에서 오늘날짜까지 근무 일수(소수점까지 나옴)
+SELECT EMP_NAME, HIRE_DATE, SYSDATE - HIRE_DATE 근무일수
+FROM EMPLOYEE;
+
+-- EMPLOYEE에서 오늘날짜까지 근무 일수(정수)
+SELECT EMP_NAME, HIRE_DATE, CEIL(SYSDATE - HIRE_DATE) 근무일수
+FROM EMPLOYEE;
+
+-- EMPLOYEE에서 오늘날짜까지 근무 개월수(소수점까지 나옴)
+SELECT EMP_NAME, HIRE_DATE, MONTHS_BETWEEN(SYSDATE, HIRE_DATE) 근무개월수
+FROM EMPLOYEE;
+
+-- EMPLOYEE에서 오늘날짜까지 근무 개월수(정수)
+SELECT EMP_NAME, HIRE_DATE, CEIL(MONTHS_BETWEEN(SYSDATE, HIRE_DATE)) || '개월차' 근무개월수
+FROM EMPLOYEE;
+
+---------------------------------------------------------------------------------------------------------
+/*
+    * ADD_MONTHS(DATE, NUMBER) : 특정 날짜에 해당 숫자만큼의 개월수를 더해 그 날짜를 반환
+*/
+SELECT ADD_MONTHS(SYSDATE, 1) FROM DUAL;
+
+-- EMPLOYEE테이블에서 사원명, 입사일, 정직원이 된 날짜(입사일에서 6개월후) 조회
+SELECT EMP_NAME, HIRE_DATE, ADD_MONTHS(HIRE_DATE, 6)
+FROM EMPLOYEE;
+
+---------------------------------------------------------------------------------------------------------
+/*
+    * NEXT_DAY(DATE, 요일(문자|숫자)) : 특정 날짜 이후에 가까운 해당 요일의 날짜를 반환해주는 함수
+*/
+SELECT SYSDATE, NEXT_DAY(SYSDATE, '월요일') FROM DUAL;
+SELECT SYSDATE, NEXT_DAY(SYSDATE, '금요일') FROM DUAL;
+SELECT SYSDATE, NEXT_DAY(SYSDATE, '금') FROM DUAL;
+
+-- 1: 일요일
+SELECT SYSDATE, NEXT_DAY(SYSDATE, 2) FROM DUAL;
+
+SELECT SYSDATE, NEXT_DAY(SYSDATE, 'FRIDAY') FROM DUAL;  -- 오류 현재 언어가 KOREA이기 때문
+
+-- 언어변경
+ALTER SESSION SET NLS_LANGUAGE = AMERICAN;
+SELECT SYSDATE, NEXT_DAY(SYSDATE, 'FRIDAY') FROM DUAL; 
+SELECT SYSDATE, NEXT_DAY(SYSDATE, '금') FROM DUAL;   -- 언어를 바꿨기 때문에 오류
+
+ALTER SESSION SET NLS_LANGUAGE = KOREAN;
+
+---------------------------------------------------------------------------------------------------------
+/*
+    * LAST_DAY(DATE) : 해당 월의 마지막 날짜를 반환해주는 함수
+*/
+SELECT LAST_DAY(SYSDATE) FROM DUAL;
+
+-- EMPLOYEE테이블에서 사원명, 입사일, 입사한 달의 마지막 날짜 조회
+SELECT EMP_NAME, HIRE_DATE, LAST_DAY(HIRE_DATE)
+FROM EMPLOYEE;
+
+---------------------------------------------------------------------------------------------------------
+/*
+    * EXTRACT : 특정날짜로 부터 년|월|일 값을 추출하여 반환해주는 함수 (반환형 : NUMBER)
+    
+    EXTRACT(YEAR FROM DATE) : 년도만 추출
+    EXTRACT(MONTH FROM DATE) : 월만 추출
+    EXTRACT(DAY FROM DATE) : 일만 추출
+*/
+
+-- EMPLOYEE테이블에서 사원명, 입사년도, 입사월, 입사일 조회
+SELECT EMP_NAME
+        , EXTRACT(YEAR FROM HIRE_DATE) 입사년도
+        , EXTRACT(MONTH FROM HIRE_DATE) 입사월
+        , EXTRACT(DAY FROM HIRE_DATE) 입사일
+FROM EMPLOYEE
+-- ORDER BY 2, 3, 4;
+ORDER BY 입사년도, 입사월, 입사일;
+
+--==============================================================
+--                                                      <형변환 함수>
+--==============================================================
+/*
+    * TO_CHAR : 숫자나 날짜를 문자 타입으로 변환시켜주는 함수
+    
+      TO_CHAR(숫자|날짜, [포맷])
+*/
+--------------------------------------- 숫자 타입 => 문자 타입
+/*
+    9 : 해당 자리의 숫자를 의미한다.
+        - 값이 없을 경우 소수점 이상은 공백, 소수점 이하는 0으로 표시
+    0 : 해당 자리의 숫자를 의미한다.
+        - 값이 없을 경우 0으로 표시하며, 숫자의 길이를 고정적으로 표시할 때 주로 사용
+    FM : 해당자리에 값이 없을 경우 자리차지하지 않음.
+*/
+SELECT TO_CHAR(1234) FROM DUAL;
+SELECT TO_CHAR(1234, '999999') FROM DUAL;    -- 6칸 공간 확보, 왼쪽 정렬 빈칸은 공백
+SELECT TO_CHAR(1234, '000000') FROM DUAL;    -- 6칸 공간 확보, 왼쪽 정렬 빈칸은 0으로 채움
+SELECT TO_CHAR(1234, 'L99999') FROM DUAL;    -- 현재 설정된 나라(LOCAL)의 화폐단위(빈칸 공백) : 오른쪽 정렬
+
+SELECT TO_CHAR(1234, 'L99,999') FROM DUAL;    -- 3자리마다 ,(컴마) 
+
+-- EMPLOYEE테이블에서 사원명, 급여(\11,111,111), 연봉(\111,111,111)
+SELECT EMP_NAME, TO_CHAR(SALARY, 'L99,999,999') 급여, TO_CHAR(SALARY*12, 'L999,999,999') 연봉
+FROM EMPLOYEE;
+
+SELECT TO_CHAR(123.456, 'FM999990.999')
+        , TO_CHAR(1234.56, 'FM9990.99')
+        , TO_CHAR(0.1000, 'FM9999.999')
+        , TO_CHAR(0.1000, 'FM9990.999')
+FROM DUAL;
+
+SELECT TO_CHAR(123.456, '999990.999')
+        , TO_CHAR(1234.56, '9990.99')
+        , TO_CHAR(0.1000, '9999.999')
+        , TO_CHAR(0.1000, '9990.999')
+FROM DUAL;
+
+--------------------------------------- 날짜 타입 => 문자 타입
+-- 시간
+SELECT TO_CHAR(SYSDATE, 'AM HH:MI:SS') FROM DUAL;  -- 12시간제 형식
+SELECT TO_CHAR(SYSDATE, 'HH24:MI:SS') FROM DUAL;        -- 24시간제 형식
+
+-- 날짜
+SELECT TO_CHAR(SYSDATE, 'YYYY-MM-DD DAY DY') FROM DUAL;
+SELECT TO_CHAR(SYSDATE, 'MON, YYYY') FROM DUAL;
+
+SELECT TO_CHAR(SYSDATE, 'YYYY"년 " MM"월 " DD"일 " DAY') FROM DUAL;
+SELECT TO_CHAR(SYSDATE, 'DL') FROM DUAL;
+
+-- EMPLOYEE테이블에서 입사일(25-03-20)
+SELECT TO_CHAR(HIRE_DATE, 'YY-MM-DD')
+FROM EMPLOYEE;
+
+-- EMPLOYEE테이블에서 입사일(2024년 3월 20일 화요일)
+SELECT TO_CHAR(HIRE_DATE, 'DL')
+FROM EMPLOYEE;
+
+-- EMPLOYEE테이블에서 입사일(25년 03월 20일)
+SELECT TO_CHAR(HIRE_DATE, 'YY"년 " MM"월 " DD"일" ')
+FROM EMPLOYEE;
+
+-- 년도
+/*
+    yy 는 무조건 앞에 '20'이 붙는다
+    rr 는 50년을 기준으로 50보다 작으면 앞에 '20'붙이고, 50보다 크면 '19'를 붙인다
+*/
+SELECT TO_CHAR(SYSDATE, 'YYYY')
+        , TO_CHAR(SYSDATE, 'YY')
+        , TO_CHAR(SYSDATE, 'RRRR')
+        , TO_CHAR(SYSDATE, 'RR')
+FROM DUAL;
+
+-- 월
+SELECT TO_CHAR(SYSDATE, 'MM') 
+        , TO_CHAR(SYSDATE, 'MON')       -- 영문일때 3글자만
+        , TO_CHAR(SYSDATE, 'MONTH')     -- 영문일때는 다출력
+        , TO_CHAR(SYSDATE, 'RM')            -- 로마자로
+FROM DUAL;
+
+-- 일
+SELECT TO_CHAR(SYSDATE, 'DDD')      -- 년을 기준으로 몇일째
+        , TO_CHAR(SYSDATE, 'DD')       -- 월을 기준으로 몇일째
+        , TO_CHAR(SYSDATE, 'D')         -- 주를 기준으로 몇일째
+FROM DUAL;
+
+-- 요일
+SELECT TO_CHAR(SYSDATE, 'DAY')      
+        , TO_CHAR(SYSDATE, 'DY')     
+FROM DUAL;
+
+---------------------------------------------------------------------------------------------------------
+/*
+    * TO_DATE : 숫자 또는 문자 타입을 날짜 타입으로 변환
+    
+    TO_DATE(숫자|문자, [포맷])
+*/
+
+SELECT TO_DATE(20241230) FROM DUAL;
+SELECT TO_DATE(241230) FROM DUAL;
+
+-- SELECT TO_DATE(050101) FROM DUAL;  -- 앞이 0일때 오류
+SELECT TO_DATE('050101') FROM DUAL;   --앞이 0일때는 문자형태로 넣어줘야 한다
+
+-- SELECT TO_DATE('070204 142530', 'YYMMDD HHMISS') FROM DUAL;  -- 오류 12시간제로 출력 14시는 없음
+-- SELECT TO_DATE('070204 142530', 'YYMMDD HH24MISS') FROM DUAL;  -- 변환후 사용
+
+SELECT TO_CHAR(TO_DATE('070204 022530', 'YYMMDD HHMISS'), 'YY-MM-DD HH:MI:SS') FROM DUAL;
+
+SELECT TO_DATE('040325', 'YYMMDD') FROM DUAL;  
+SELECT TO_DATE('980630', 'YYMMDD') FROM DUAL;   -- 현재 세기로 반영
+
+SELECT TO_DATE('040325', 'RRMMDD') FROM DUAL; 
+SELECT TO_DATE('980630', 'RRMMDD') FROM DUAL;   -- 50미만일 경우 현재세기, 50이상이면 이전 세기 반영
+
+---------------------------------------------------------------------------------------------------------
+/*
+    * TO_NUMBER : 문자타입을 숫자타입으로 변환
+    
+      TO_NUMBER(문자, [포맷])
+*/
+SELECT TO_NUMBER('012341234') FROM DUAL;
+SELECT '1000000' + '5000000' FROM DUAL;         -- 오라클은 자동 형변환 됨
+-- SELECT '1,000,000' + '5,000,000' FROM DUAL;         -- 오류 :  특수문자가 포함되어 문자임. 자동형변환 안됨 
+SELECT TO_NUMBER('1,000,000', '9,999,999') + TO_NUMBER('5,000,000', '9,999,999') FROM DUAL;
+
+--==============================================================
+--                                                      <NULL 처리 함수>
+--==============================================================
+/*
+    NVL(컬럼, 해당컬럼이 NULL일경우 반환할 값)
+*/
+
+SELECT EMP_NAME, BONUS, NVL(BONUS, 0)
+FROM EMPLOYEE;
+
+-- 전 사원의 이름, 보너스포함 연봉
+SELECT EMP_NAME, (SALARY*(1+BONUS)) * 12
+FROM EMPLOYEE;
+
+SELECT EMP_NAME, SALARY*(1+ NVL(BONUS, 0)) * 12
+FROM EMPLOYEE;
+
+SELECT EMP_NAME, SALARY*NVL(1+BONUS, 1) * 12  -- 곱하기니까 0이 아닌 1로 해야됨.
+FROM EMPLOYEE;
+
+--  사원명, 부서코드(부서가 없으면 '부서없음')
+SELECT EMP_NAME, NVL(DEPT_CODE, '부서없음')
+FROM EMPLOYEE;
+
+---------------------------------------------------------------------------------------------------------
+/*
+    * NVL2(컬럼, 반환값1, 반환값2)
+      - 컬럼에 값이 존재하면 반환값1
+      - 컬럼에 값이 존재하지 않으면 반환값2
+*/
+
+--  사원명, 부서(부서에 속해있으면 '부서있음', 부서가 없으면 '부서없음')
+SELECT EMP_NAME, NVL2(DEPT_CODE, '부서있음', '부서없음')
+FROM EMPLOYEE;
+
+-- EMPLOYEE테이블에서 사원명, 급여, 보너스, 성과급(보너스를 받는사람은 50%를 주고, 보너스를 못받는 사람은 10%)
+SELECT EMP_NAME, SALARY, BONUS, SALARY*NVL2(BONUS, 0.5, 0.1) 성과급
+FROM EMPLOYEE;
  
  
